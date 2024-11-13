037e09a828e9461f16739b0ef177c15f62e6137b
Thomas Sioutas <thsioutas@gmail.com>
2024-11-13 21:27:10 +0100

    Add reset_total_transferred instruction and implement some fixes
    	Also update test client accordingly

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,13 @@
-use borsh::BorshSerialize;
 use clap::Parser;
 use solana_client::rpc_client::RpcClient;
+use solana_example_program::SolanaExampleInstruction;
 use solana_sdk::{
     instruction::{AccountMeta, Instruction},
     signature::{read_keypair_file, Keypair, Signer},
     system_program,
     transaction::Transaction,
 };
+use std::io::{Read, Write};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -21,6 +22,25 @@ struct Args {
     /// The file containing the payer's keypair.
     #[arg(long, default_value_t = payer_default())]
     payer: String,
+
+    /// The file containing the state account's keypair.
+    #[arg(long, default_value_t = state_account_default())]
+    state_account: String,
+
+    #[command(flatten)]
+    pay_or_reset: PayOrReset,
+}
+
+#[derive(Parser, Debug)]
+#[group(required = true, multiple = false)]
+struct PayOrReset {
+    /// Transfer the specified amount
+    #[arg(short, long)]
+    transfer: Option<u64>,
+
+    /// Reset the total transferred amount
+    #[arg(short, long)]
+    reset: bool,
 }
 
 fn payer_default() -> String {
@@ -29,9 +49,10 @@ fn payer_default() -> String {
     home_dir.to_str().unwrap().to_string()
 }
 
-#[derive(BorshSerialize, Debug)]
-struct TransferInstruction {
-    amount: u64,
+fn state_account_default() -> String {
+    let mut home_dir = dirs::home_dir().unwrap();
+    home_dir.push(".config/solana/solana-example-state-keypair.json");
+    home_dir.to_str().unwrap().to_string()
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -39,32 +60,72 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set up Solana RPC client to talk to Localnet
     let client = RpcClient::new("http://localhost:8899".to_string());
 
-    // Program ID of the deployed program
-    let program_id = read_keypair_file(args.program)?.pubkey();
+    let program = read_keypair_file(args.program)?;
     let payer = read_keypair_file(args.payer)?;
     let recipient = Keypair::new();
-    let state_account = Keypair::new();
+
+    let state_account_keypair_path = args.state_account;
+    let state_account = if std::path::PathBuf::from(&state_account_keypair_path).exists() {
+        let mut file = std::fs::File::open(state_account_keypair_path)?;
+        let mut keypair_bytes = Vec::new();
+        file.read_to_end(&mut keypair_bytes)?;
+        Keypair::from_bytes(&keypair_bytes).unwrap()
+    } else {
+        println!(
+            "Generate new state account keypair and store in: {:?}",
+            state_account_keypair_path
+        );
+        let state_account = Keypair::new();
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(state_account_keypair_path)?;
+        file.write_all(&state_account.to_bytes()).unwrap();
+        state_account
+    };
     let system_account = system_program::ID;
+    // Fund the payer account on localnet
+    client.request_airdrop(&payer.pubkey(), 1_000_000_000)?;
 
-        // Fund the payer account on localnet
-        client.request_airdrop(&payer.pubkey(), 1_000_000_000)?;
+    if let Some(amount) = args.pay_or_reset.transfer {
+        let data = SolanaExampleInstruction::Transfer { amount };
+        println!(
+            "Prepare and send a new sol_transfer instruction with data = {:?}",
+            data
+        );
+        let sol_transfer_ix = Instruction::new_with_borsh(
+            program.pubkey(),
+            &data,
+            vec![
+                AccountMeta::new(payer.pubkey(), true),
+                AccountMeta::new(recipient.pubkey(), true),
+                AccountMeta::new(state_account.pubkey(), true),
+                AccountMeta::new_readonly(system_account, false),
+            ],
+        );
 
-    let data = TransferInstruction { amount: 2 };
-    // Create and send the "sol_transfer" transaction
-    let sol_transfer_ix = Instruction::new_with_borsh(
-        program_id,
-        &data,
-        vec![
-            AccountMeta::new(payer.pubkey(), true),
-            AccountMeta::new(recipient.pubkey(), true),
-            AccountMeta::new(state_account.pubkey(), true),
-            AccountMeta::new_readonly(system_account, false),
-        ],
-    );
+        let recent_blockhash = client.get_latest_blockhash()?;
+        let mut transaction =
+            Transaction::new_with_payer(&[sol_transfer_ix], Some(&payer.pubkey()));
+        transaction.sign(&[&payer, &recipient, &state_account], recent_blockhash);
+        client.send_and_confirm_transaction(&transaction)?;
+    } else {
+        let data = SolanaExampleInstruction::Reset;
+        let reset_transferred_ix = Instruction::new_with_borsh(
+            program.pubkey(),
+            &data,
+            vec![
+                AccountMeta::new(payer.pubkey(), true),
+                AccountMeta::new(state_account.pubkey(), true),
+            ],
+        );
+        let recent_blockhash = client.get_latest_blockhash()?;
+        let mut transaction =
+            Transaction::new_with_payer(&[reset_transferred_ix], Some(&payer.pubkey()));
+        transaction.sign(&[&payer, &state_account], recent_blockhash);
+        client.send_and_confirm_transaction(&transaction)?;
+    }
 
-    let recent_blockhash = client.get_latest_blockhash()?;
-    let mut transaction = Transaction::new_with_payer(&[sol_transfer_ix], Some(&payer.pubkey()));
-    transaction.sign(&[&payer, &recipient, &state_account], recent_blockhash);
-    client.send_and_confirm_transaction(&transaction)?;
     Ok(())
 }
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -11,11 +11,14 @@ use solana_program::{
     system_instruction,
     sysvar::Sysvar,
 };
+use std::io::Write;
+
 entrypoint!(process_instruction);
 
-#[derive(BorshDeserialize, Debug)]
-struct TransferInstruction {
-    amount: u64,
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub enum SolanaExampleInstruction {
+    Transfer { amount: u64 },
+    Reset,
 }
 
 /// Persistent state structure to track total transferred SOL.
@@ -29,8 +32,13 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let transfer_instruction = TransferInstruction::try_from_slice(instruction_data)?;
-    sol_transfer(accounts, transfer_instruction, program_id)
+    use SolanaExampleInstruction::*;
+    let transfer_instruction = SolanaExampleInstruction::try_from_slice(instruction_data)?;
+    msg!("Instruction = {:?}", transfer_instruction);
+    match transfer_instruction {
+        Transfer { amount } => sol_transfer(accounts, amount, program_id),
+        Reset => reset_total_transferred(accounts),
+    }
 }
 
 /// Ensures that the state account is created and initialized if it doesn't already exist.
@@ -41,12 +49,9 @@ fn ensure_state_account<'a>(
     system_program: &AccountInfo<'a>,
 ) -> ProgramResult {
     if state_account.lamports() == 0 {
-        // The extra +8 bytes are needed because, in Solana, account data includes a Borsh serialization prefix.
-        // Borsh, the serialization format used here, stores serialized data with a length prefix that indicates
-        // the size of the data. This prefix is typically 8 bytes long for larger structures like TransferState.
-        let account_space = std::mem::size_of::<TransferState>() + 8;
+        let account_space = std::mem::size_of::<TransferState>();
         // Calculate the minimum lamports required for rent exemption
-        let rent_exempt_balance =  Rent::get()?.minimum_balance(account_space);
+        let rent_exempt_balance = Rent::get()?.minimum_balance(account_space);
 
         // Create the state account with the minimum required lamports
         msg!("Creating state account for tracking total transferred SOL");
@@ -65,7 +70,14 @@ fn ensure_state_account<'a>(
         let state = TransferState {
             total_transferred: 0,
         };
-        state.serialize(&mut *state_account.data.borrow_mut())?;
+
+        let mut serialized_data: Vec<u8> = Vec::new();
+        state.serialize(&mut serialized_data).unwrap();
+        let mut state_data = state_account.try_borrow_mut_data()?;
+        (&mut state_data[..serialized_data.len()]).write_all(&serialized_data)?;
+
+        // TODO: Why doesn't this work?
+        // state.serialize(&mut *state_account.try_borrow_mut_data()?)?;
 
         msg!("State account created and initialized");
     } else {
@@ -84,7 +96,7 @@ fn ensure_recipient_account<'a>(
         // Size of data: 0 because it's a system account, which holds only lamports
         let account_space = 0;
         // Calculate the minimum lamports required for rent exemption
-        let rent_exempt_balance =  Rent::get()?.minimum_balance(account_space);
+        let rent_exempt_balance = Rent::get()?.minimum_balance(account_space);
 
         // Create the recipient account as a system account
         invoke(
@@ -93,7 +105,7 @@ fn ensure_recipient_account<'a>(
                 recipient.key,
                 rent_exempt_balance,
                 account_space as u64,
-                &system_program.key,
+                system_program.key,
             ),
             &[payer.clone(), recipient.clone(), system_program.clone()],
         )?;
@@ -104,19 +116,13 @@ fn ensure_recipient_account<'a>(
     Ok(())
 }
 
-fn sol_transfer(
-    accounts: &[AccountInfo],
-    instruction: TransferInstruction,
-    program_id: &Pubkey,
-) -> ProgramResult {
+fn sol_transfer(accounts: &[AccountInfo], amount: u64, program_id: &Pubkey) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
 
     let payer = next_account_info(accounts_iter)?;
     let recipient = next_account_info(accounts_iter)?;
     let state_account = next_account_info(accounts_iter)?;
     let system_program = next_account_info(accounts_iter)?;
-
-    // msg!("Start {} SOL transfer from {} to {}",  instruction.amount, payer.key, recipient.key,);
 
     // Ensure the recipient account exists
     ensure_recipient_account(payer, recipient, system_program)?;
@@ -125,7 +131,7 @@ fn sol_transfer(
     ensure_state_account(payer, state_account, program_id, system_program)?;
 
     // Check if the payer has enough funds for the transfer
-    if payer.lamports() < instruction.amount {
+    if payer.lamports() < amount {
         msg!("Insufficient funds");
         return Err(ProgramError::InsufficientFunds);
     }
@@ -137,26 +143,47 @@ fn sol_transfer(
 
     msg!(
         "Transferring {} lamports from {} to {}",
-        instruction.amount,
+        amount,
         payer.key,
         recipient.key
     );
 
     invoke(
-        &system_instruction::transfer(payer.key, recipient.key, instruction.amount),
+        &system_instruction::transfer(payer.key, recipient.key, amount),
         &[payer.clone(), recipient.clone(), system_program.clone()],
     )?;
 
     msg!("Transfer successful");
-
-    let mut state_data = state_account.try_borrow_mut_data()?;
-    let mut state = TransferState::try_from_slice(&state_data)?;
-    state.total_transferred += instruction.amount;
-    state.serialize(&mut *state_data)?;
+    let mut state = TransferState::try_from_slice(&state_account.try_borrow_mut_data()?)?;
+    state.total_transferred += amount;
+    state.serialize(&mut *state_account.data.borrow_mut())?;
 
     msg!(
         "State updated successfully, total_transferred: {}",
         state.total_transferred
     );
+    Ok(())
+}
+
+/// Resets the total transferred amount; only the admin can do this.
+fn reset_total_transferred(accounts: &[AccountInfo]) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    // Retrieve the necessary accounts
+    let admin = next_account_info(accounts_iter)?;
+    let state_account = next_account_info(accounts_iter)?;
+
+    // Check that the admin is a signer
+    if !admin.is_signer {
+        msg!("Admin must be a signer to reset the state");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    // Reset the total transferred state
+    let mut state_data = state_account.try_borrow_mut_data()?;
+    let mut state = TransferState::try_from_slice(&state_data)?;
+    state.total_transferred = 0;
+    state.serialize(&mut *state_data)?;
+
+    msg!("Total transferred amount has been reset by the admin");
     Ok(())
 }
